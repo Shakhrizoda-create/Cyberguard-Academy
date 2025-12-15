2bb83d997988aaea3901de9a33f337293b097941
Shakhrizoda-create <shakhrizodasamaye@gmail.com>
2025-12-15 22:09:29 +0500

    Update spot-the-phish.js

--- spot-the-phish/spot-the-phish.js
+++ spot-the-phish/spot-the-phish.js
@@ -1,10 +1,10 @@
 // ================= BACKGROUND MUSIC =================
 const bgMusic = new Audio("assets-3/singularity-abstract-electronica-281092.mp3");
 bgMusic.loop = true;
-bgMusic.volume = 0.25;
+bgMusic.volume = 1;
 
 const clickSound = new Audio("assets-3/computer-mouse-click-352734.mp3");
-clickSound.volume = 0.5;
+clickSound.volume = 0.75;
 
 // ================= ELEMENTS =================
 const startScreen = document.getElementById("start-screen");
